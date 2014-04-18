42d7b4e22ae058766549f82aeb1e11ce7ae5fbac
Richard Baker <richiebkr@gmail.com>
2014-04-18 10:40:42 -0700

    built layout: added buttons, added time changing functionality

--- alarm-clock.js
+++ alarm-clock.js
@@ -1,10 +1,32 @@
 (function( window, undefined ){
 
-  var currentTime = new Date(),
-      currentHours = currentTime.getHours(),
-      currentMinutes = currentTime.getMinutes(),
-      currentSeconds = currentTime.getSeconds();
-  console.log(currentTime);
+  setInterval(function updateClock() {
+    if (window.addEventListener) {
+      window.addEventListener('load', updateClock, false);
+    }
+    else if (window.attachEvent) {
+      window.attachEvent('onload', updateClock);
+    }
 
+    var currentTime = new Date(),
+        currentHours = currentTime.getHours(),
+        currentMinutes = currentTime.getMinutes(),
+        currentSeconds = currentTime.getSeconds();
+        currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+        currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+        currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+        currentHours = (currentHours == 0) ? 12 : currentHours;
+        var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+        var currentTimeString = currentHours
+                              + ":"
+                              + currentMinutes
+                              + ":"
+                              + currentSeconds
+                              + " "
+                              + timeOfDay;
+    document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+  }, 1000);
+
+  var controlPanel = document.getElementById("")
 
 })(window);
